d0ff421e6d31f8ff59bdc5c7980ecf027774cfb2
Serena Chuang <tzyharnchuang@gmail.com>
2024-11-21 16:41:18 -0800

    desktop stats done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Statistics from '@/sections/Statistics';
 import styles from './page.module.scss';
-import FAQs from '@/sections/FAQs'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Statistics/>
-      <FAQs />
     </main>
   );
 }
