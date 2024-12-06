68d550f0f7ff78dfab9e023e478e86da37a37563
Serena Chuang <tzyharnchuang@gmail.com>
2024-12-05 17:19:48 -0800

    stats updates, fixed sliding diamong img and section gaps

--- src/sections/Statistics/index.tsx
+++ src/sections/Statistics/index.tsx
@@ -2,12 +2,13 @@ import StatsDescription from '@/components/StatsDescription';
 import StatsNumbers from '@/components/StatsNumbers';
 import styles from './style.module.scss';
 import Image from 'next/image';
+import SlidingDiamond from 'public/sliding_diamond.png'
 
 const Statistics: React.FC = () => {
     return (
         <section className={styles.container}>
             <div className={styles.diamond}>
-                <Image src="/sliding_diamond.png" 
+                <Image src={SlidingDiamond} 
                     alt="sliding diamond"
                     layout="fill" 
                     objectFit="contain"
