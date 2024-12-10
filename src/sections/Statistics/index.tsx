5884266bbd5202b462bac1ef71f6ecd599013221
Alex Zhang <alexzhang1618@gmail.com>
2024-12-09 21:42:15 -0800

    bump quality

--- src/sections/Statistics/index.tsx
+++ src/sections/Statistics/index.tsx
@@ -1,13 +1,19 @@
 import StatsDescription from '@/components/StatsDescription';
 import styles from './style.module.scss';
+import SlidingDiamond from 'public/assets/sliding-diamond.png';
 import Image from 'next/image';
-import SlidingDiamond from 'public/assets/sliding_diamond.png';
 
 const Statistics: React.FC = () => {
   return (
     <section className={styles.container}>
       <div className={styles.diamond}>
-        <Image src={SlidingDiamond} alt="sliding diamond" className={styles.slidingDiamond} />
+        <Image
+          src={SlidingDiamond}
+          alt="Sliding diamond"
+          className={styles.slidingDiamond}
+          width={574}
+          height={896}
+        />
       </div>
       <div className={styles.innerContainer}>
         <StatsDescription />
